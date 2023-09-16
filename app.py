40d9f03b01f5b98c644aaab2e9533e21e7504322
USERNAME <MAIL>
2023-09-16 13:49:40 +0000

    Updated code

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 # Import necessary libraries
 import streamlit as st
 from langchain import HuggingFacePipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 # Function to load the language model
 @st.cache_resource
 def load_language_model():
-    return HuggingFacePipeline.from_model_id(model_id="rinna/japanese-gpt2-small", task="text-generation")
+    tokenizer = AutoTokenizer.from_pretrained("rinna/japanese-gpt2-small", use_fast=False)
+    tokenizer.do_lower_case = True  # due to some bug of tokenizer config loading
+    model = AutoModelForCausalLM.from_pretrained("rinna/japanese-gpt2-small")
+    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=64)
+    llm = HuggingFacePipeline(pipeline=pipe)
+    return llm
+    #return HuggingFacePipeline.from_model_id(model_id="rinna/japanese-gpt2-small", task="text-generation")
 
 # Display a loading spinner
 with st.spinner('Please wait...'):
