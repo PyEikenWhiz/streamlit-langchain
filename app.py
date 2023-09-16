dd6d8b515d3dddd4f50bf678f9d6ffd815c2c58a
USERNAME <MAIL>
2023-09-16 11:54:29 +0000

    Updated code

--- app.py
+++ app.py
@@ -2,10 +2,14 @@
 import streamlit as st
 from langchain import HuggingFacePipeline
 
+# Create a Streamlit app
+st.title("Text Generation with GPT-2")
+
 # Function to load the language model
 @st.cache_resource
 def load_language_model():
-    return HuggingFacePipeline.from_model_id(model_id="gpt2", task="text-generation")
+    language_model = HuggingFacePipeline.from_model_id(model_id="gpt2", task="text-generation")
+    return language_model
 
 # Display a loading spinner
 with st.spinner('Please wait...'):
@@ -13,5 +17,11 @@ with st.spinner('Please wait...'):
     language_model = load_language_model()
     st.write("Done!")
 
-# Generate and display text
-st.write(language_model("Once upon a time, "))
+# User input for text generation
+user_input = st.text_input("Enter a starting phrase:", "Once upon a time,")
+
+# Generate and display text based on user input
+if user_input:
+    generated_text = language_model(user_input)
+    st.write("Generated text:")
+    st.write(generated_text)
