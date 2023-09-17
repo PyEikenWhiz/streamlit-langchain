530e0c7ad6825a466b0ab28207c2347c2a3d2bf5
USERNAME <MAIL>
2023-09-17 11:51:06 +0000

    Updated code

--- app.py
+++ app.py
@@ -1,39 +1,22 @@
 # Import necessary libraries
-import streamlit as st  # Import the Streamlit library for building web apps
-from langchain import HuggingFacePipeline  # Import the language model library
-import time  # Import the time module to measure execution time
-
-# Record the start time
-start_time = time.time()
-
-# Create a Streamlit app with a title
-st.title("Text Generation with GPT-2")
+import streamlit as st
+from langchain import HuggingFacePipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 # Function to load the language model
-#@st.cache_resource  # This decorator caches the result for better performance
+@st.cache_resource
 def load_language_model():
-    # Load the language model using HuggingFacePipeline
-    language_model = HuggingFacePipeline.from_model_id(model_id="gpt2", task="text-generation")
-    return language_model
+    model_id = "gpt2"
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(model_id)
+    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=64)
+    return HuggingFacePipeline(pipeline=pipe)
 
-# Display a loading spinner while the language model is loaded
+# Display a loading spinner
 with st.spinner('Please wait...'):
     # Load the language model
     language_model = load_language_model()
-    st.write("Done!")  # Indicate that the loading is complete
+    st.write("Done!")
 
-# User input for text generation
-user_input = st.text_input("Enter a starting phrase:", "Once upon a time,")
-
-# Generate and display text based on user input
-if user_input:
-    generated_text = language_model(user_input)  # Generate text
-    st.write("Generated text:")  # Display a label for the generated text
-    st.write(generated_text)  # Display the generated text
-
-# Record the end time
-end_time = time.time()
-
-# Calculate and display the execution time
-execution_time = end_time - start_time
-st.write(f"Execution time: {execution_time:.2f} seconds")
+# Generate and display text
+st.write(language_model("Once upon a time, "))
