9c5b21fb287b76a038ddc1512691a9a8167b3ed3
USERNAME <MAIL>
2023-09-18 08:27:18 +0000

    Updated code

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
-# Import necessary libraries
 import streamlit as st
-from langchain import HuggingFacePipeline
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
-# Function to load the language model
+# Import the required class #!pip install transformers
+from langchain import PromptTemplate, LLMChain
+
+# Define the template with a variable named "subject"
+template = "Tell me about {subject}"
+# Create an instance of PromptTemplate and specify the variable names using input_variables
+prompt = PromptTemplate(template=template, input_variables=["subject"])######################
+# Use the format method to replace the variable "subject" with a specific value
+prompt_text = prompt.format(subject="IT enginner")
+# Print the resulting text
+#print(prompt_text)
+st.write("prompt: ",prompt_text)
+
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from langchain.llms import HuggingFacePipeline
+
+#from LLM (HuggingFacePipeline)
 @st.cache_resource
 def load_language_model():
-    tokenizer = AutoTokenizer.from_pretrained("rinna/japanese-gpt2-small", use_fast=False)
+    model_id = "rinna/japanese-gpt2-small"
+    tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=False)
     tokenizer.do_lower_case = True  # due to some bug of tokenizer config loading
-    model = AutoModelForCausalLM.from_pretrained("rinna/japanese-gpt2-small")
+    model = AutoModelForCausalLM.from_pretrained(model_id)
     pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=64)
     llm = HuggingFacePipeline(pipeline=pipe)
     return llm
-    #return HuggingFacePipeline.from_model_id(model_id="rinna/japanese-gpt2-small", task="text-generation")
 
-language_model = load_language_model()
-
-# Generate and display text
-st.write(language_model("生命、宇宙、そして万物についての究極の疑問の答えは, ", num_return_sequences=6))
+#print(llm(prompt_text))
+llm = load_language_model()
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+question = "Osaka"
+st.write(llm_chain.run(question))
