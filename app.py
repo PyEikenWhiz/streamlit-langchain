c62a90fecf5f6c1a37107402edc9e85e10bcb6aa
USERNAME <MAIL>
2023-09-20 10:26:20 +0000

    Updated code

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 import streamlit as st
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import sqlite3
+
 long_text = """
 GPT-4は、OpenAIが開発したAI技術であるGPTシリーズの第4世代目のモデルです。
 
